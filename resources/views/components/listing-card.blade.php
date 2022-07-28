8e1b93f2c40a3f25267ea7e40c8009361762e1c7
abdulshakur <abdulshakur486@gmail.com>
2022-07-28 09:58:25 +0300

    customed.

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -3,7 +3,7 @@
 <x-card>
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block"
-            src="{{ asset($listing->logo != null ? 'storage/' . $listing->logo : 'images/no-image.png') }}"
+            src="{{ asset($listing->logo != null ? 'storage/' . $listing->logo : 'images/no-image.svg') }}"
             alt="" />
 
         <div>
@@ -12,8 +12,9 @@
             </h3>
             <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
             <x-listing-tags :tagsCsv="$listing->tags" />
-            <div class="text-lg mt-4">
-                <i class="fa-solid fa-location-dot">{{ $listing->location }}</i>
+            <div class="text-lg mt-4 flex items-center justify-start gap-2">
+                <i class="fa-solid fa-location-dot"></i>
+                <p>{{ $listing->location }}</p>
             </div>
         </div>
     </div>
