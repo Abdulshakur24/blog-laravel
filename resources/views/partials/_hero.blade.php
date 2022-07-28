2c4e020484222ca5a55faac532bafffbb911a121
abdulshakur <abdulshakur486@gmail.com>
2022-07-28 18:00:57 +0300

    customed.

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,4 +1,4 @@
-<section class="relative h-72 bg-primary flex flex-col justify-center align-center text-center space-y-4 mb-4">
+<section class="relative h-72 bg-black flex flex-col justify-center align-center text-center space-y-4 mb-4">
     <div class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
         style="background-image: url('/images/no-image.svg');"></div>
 
@@ -11,11 +11,12 @@
         </p>
         <div>
             @auth
-            < @else <a href="/register"
+            @else
+                <a href="/register"
                     class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">
                     Sign
-                    Up to List a Gig</a>
-                @endauth
+                    Up to List a post</a>
+            @endauth
 
 
         </div>
