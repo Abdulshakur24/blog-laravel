917eaf5443dba277c8e104b8035f90571d25a9eb
abdulshakur <abdulshakur486@gmail.com>
2022-07-28 09:56:57 +0300

    customed.

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,7 +1,7 @@
 @if (session()->has('message'))
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 4000)" x-show="show"
-        class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3 z-40">
-        <p>
+        class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-primary text-white p-4 z-40">
+        <p class="text-center">
             {{ session('message') }}
         </p>
     </div>
