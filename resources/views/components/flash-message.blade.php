16ddc61998d9f0dd4193ffe528c09dca0899ae55
abdulshakur <abdulshakur486@gmail.com>
2022-07-30 20:07:01 +0300

    customed.

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,6 +1,6 @@
 @if (session()->has('message'))
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 4000)" x-show="show"
-        class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-primary text-white p-4 z-40">
+        class="fixed top-4 left-1/2 transform -translate-x-1/2 bg-primary text-white p-4 z-40 rounded">
         <p class="text-center">
             {{ session('message') }}
         </p>
