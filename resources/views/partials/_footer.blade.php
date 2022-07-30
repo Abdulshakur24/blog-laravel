353035a944c331da4ecba8983f75da9dd74eaa64
abdulshakur <abdulshakur486@gmail.com>
2022-07-30 20:09:21 +0300

    initial commit.

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="max-w-[1440px] mx-auto fixed bottom-0 left-0 right-0 w-full font-bold">
+    <div class="flex items-center justify-center">
+        <p class="pl-2 text-white h-[40px] grow  bg-primary/80 flex items-center justify-center">All Rights
+            reserved.</p>
+        <button class="h-10 w-20 text-white bg-primary hover:bg-primary/80 transition ease-in-out border-0">
+            <a href="/listings/create" class="">Post
+                blog</a>
+        </button>
+    </div>
+</footer>
